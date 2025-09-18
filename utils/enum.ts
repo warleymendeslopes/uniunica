db65ddd15b3c4ac92b197a45ae56274d10a47e70
hiagoroedel <hiagojunio4@gmail.com>
2025-09-18 09:51:53 -0300

    DM-479

--- utils/enum.ts
+++ utils/enum.ts
@@ -1,6 +1,6 @@
 export enum Footer {
     teste_vocacional = "https://lp.faculdadeunica.com.br/teste-vocacional/?__hstc=132447313.c9820523b888e167606b8a5e85647b05.1753302181496.1757336165588.1757358275523.88&__hssc=132447313.1.1757358275523&__hsfp=2944137887",
-    embaixador = 'https://uniunica.edu.br/indicacao',
+    embaixador = '/indicacao',
     relatorio_transparencia_cf = 'https://storage.googleapis.com/sites-grupo-prominas/faculdade-unica/rel-igualdade-32495498000440-2025-1.pdf',
     relatorio_transparenia_ipa = 'https://storage.googleapis.com/sites-grupo-prominas/faculdade-unica/rel-igualdade-32495498000105-2025-1.pdf',
     ppc = 'https://drive.google.com/drive/folders/1nf6zRt98lfDh1t7EcIuQZM-ZeBzduzT4',
